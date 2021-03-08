8328ba3c76309204b1f5e1f66d8bbb578f299e13
mike850824 <qaz66223@gmail.com>
2021-03-09 00:54:42 +0800

    加入註解

--- weatherAPI.js
+++ weatherAPI.js
@@ -20,26 +20,28 @@ function conn(){
 		console.log(response);
 		$('#content').append('<caption>OpenWeatherAPI_Practice</caption>');
         var data = response;
-		// data[]
+		// 加入標題
 		$('#content').append('<tr><td>地名</td><td>日期</td><td>最高溫</td><td>最低溫</td><td>天氣狀況</td></tr>');
 		data.list.forEach(function(element,idx){
 			// 取得dt (時戳) 屬性
 			let timestamp = element.dt;
 			// 宣告date物件dt，代入時戳*1000
 			var dt = new Date(timestamp * 1000);
-			// 將dt轉換成月日，丟進dtpd內
 			$('#content').append('<tr>');
+			// 抓不到name就判斷無城市
 			if(data.city.name == "")
 				$('#content').append('<td>'+'此座標無城市'+'</td>');
 			else
 				$('#content').append('<td>'+ data.city.name +'</td>');
+			// 取得dt的月份即日期
 			$('#content').append('<td>'+(dt.getMonth()+1) + '月'+ dt.getDate() +'日</td>');
+			// 四捨五入最高氣溫
 			$('#content').append('<td>'+Math.round(element.temp.max-273.15)+'℃</td>');
+			// 四捨五入最低氣溫
 			$('#content').append('<td>'+Math.round(element.temp.min-273.15)+'℃</td>');
+			// 抓取天氣狀況
 			$('#content').append('<td>'+element.weather[0].main+'</td>');
 			$('#content').append('</tr>');
 		});
-
-		// $('#content').append('</table>');
-    	});
+	});
 }
